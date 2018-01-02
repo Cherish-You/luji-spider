44e1b3be8b796c462481425ed6cf309e58c11ec5
Rookie <Rookie@github.com>
2018-01-02 16:12:28 +0800

    python文件

---
+++ spider1.py
@@ -0,0 +1,24 @@
+# coding=utf-8
+from urllib import request
+import urllib
+import re
+import chardet
+
+def getHtml(url):
+    page = urllib.request.urlopen(url)
+    html = page.read()
+    return html
+
+def getImg(html):
+    reg = r'src="(.+?\.jpg)" size'
+    imgre = re.compile(reg)
+    encode_type = chardet.detect(html)
+    html = html.decode(encode_type['encoding'])
+    imglist = re.findall(imgre,html)
+    x = 0
+    for imgurl in imglist:
+        urllib.request.urlretrieve(imgurl,'%s.jpg' % x)
+        x+=1
+
+html = getHtml("https://tieba.baidu.com/p/5230780089")
+print (getImg(html))
